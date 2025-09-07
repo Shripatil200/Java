a88e816e0aaa00610bd8028814b25ff1c2a2912c
Shripatil200 <patilshriram200@gmail.com>
2025-09-08 01:35:58 +0530

    Inheritance in non generic subclass

--- src/com/example/generics/inheritance/non_generic_subclass/Storage.java
+++ src/com/example/generics/inheritance/non_generic_subclass/Storage.java
@@ -2,6 +2,9 @@ package com.example.generics.inheritance.non_generic_subclass;
 
 // Here Box is a generic class and Storage class which extends box class is non-generic class
 // Here's how we can ensure type safety.
+
+
+// We have to define which type of value it is going to take at the time of class definition.
 public class Storage extends Box<String>{
 
     public static void main(String[] args) {
