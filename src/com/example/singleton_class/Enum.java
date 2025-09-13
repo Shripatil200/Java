b943b5c24887381b912f60f0b6e3e62865820855
Shripatil200 <patilshriram200@gmail.com>
2025-09-13 23:49:44 +0530

    Enum for singleton class

--- src/com/example/singleton_class/Enum.java
+++ src/com/example/singleton_class/Enum.java
@@ -4,9 +4,6 @@ package com.example.singleton_class;
 👉 Easiest + safest (handles serialization & reflection automatically).
 👉 Enum ensures only one instance ever exists.
 
-✅ Uses class loader mechanism.
-✅ Instance created only when needed.
-✅ Thread-safe without synchronization overhead.
  */
 enum DBConnectionEnum {
     INSTANCE;
